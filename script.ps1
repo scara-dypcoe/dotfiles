a889366cc2ae0d5b588d96cb45d7e46aca2b16bc
Prathamesh Mawale <prathameshmawale@gmail.com>
2022-08-28 21:40:50 +0530

    modified powershell to install choco

--- script.ps1
+++ script.ps1
@@ -2,3 +2,9 @@
 #  here we have to setup various things list is as follows :
 #   [choco, git, linux command line commands, docker, aws cli, vscode, vscode extensions, ]
 #  have to bring uniformity amougn all members 
+
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+RefreshEnv
+
+
