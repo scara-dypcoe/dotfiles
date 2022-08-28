5dba1c867a8fa65e6d96628805a981cb751e4cff
Prathamesh Mawale <prathameshmawale@gmail.com>
2022-08-28 21:46:51 +0530

    changed ivk to invoke-expression

--- script.ps1
+++ script.ps1
@@ -3,7 +3,7 @@
 #   [choco, git, linux command line commands, docker, aws cli, vscode, vscode extensions, ]
 #  have to bring uniformity amougn all members 
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 RefreshEnv
 
