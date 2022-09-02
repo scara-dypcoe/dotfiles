0fcf1f494dbf2a4bee4e36e9a7c3dac6a364e2df
Prathamesh Mawale <prathameshmawale@gmail.com>
2022-09-02 11:18:02 +0530

    added some variables to powershell script

--- script.ps1
+++ script.ps1
@@ -2,6 +2,9 @@
 #  here we have to setup various things list is as follows :
 #   [choco, git, linux command line commands, docker, aws cli, vscode, vscode extensions, ]
 #  have to bring uniformity amougn all members 
+$email = Read-host "What is your Email?  "
+$name1 = Read-host "What is your First name?  "
+$name2 = Read-host "What is your Last name  " 
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
